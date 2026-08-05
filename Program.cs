51bc42836443a41becd60b299c652f24950b34d6
antonyessac351-debug <antonyessac@gmail.com>
2026-08-06 02:24:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace OOP_02___Smart_Delivery_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  // Question 1
+
+            //Part 01 : Theoretical Questions
+
+            //a) What is the difference between a class and a struct? 
+
+            /*
+            1- Struct:
+               - User-defined data type.
+               - Value Type.
+               - Stores its data directly (usually on the stack for local variables).
+               - Does not support inheritance from another class or struct.
+               - Best for small, lightweight data.
+
+            2- Class:
+               - User-defined data type.
+               - Reference Type.
+               - The object is stored on the heap, while the reference variable holds its address.
+               - Supports all four OOP pillars (Encapsulation, Inheritance, Polymorphism, Abstraction).
+               - Best for large and complex applications.
+            */
+
+
+            // b) Why are classes more suitable than structs for large applications?
+
+            /*
+            Classes are more suitable for large applications because:
+            1- They support all four OOP pillars:
+               - Encapsulation
+               - Inheritance
+               - Polymorphism
+               - Abstraction
+
+            2- They allow object sharing through references, which reduces unnecessary copying.
+
+            3- They are better for complex objects that contain a lot of data and behavior.
+            */
+            #endregion
+        }
+    }
+}
